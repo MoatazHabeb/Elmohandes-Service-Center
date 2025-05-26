a06ee974c30b88cb0cf095b72557be79d9e6cfdb
MoatazHabeb <moatazhabeb7@gmail.com>
2025-05-26 15:53:14 +0300

    Elmohandes-Service-Center

---
+++ Elmohandes-Service-Center/back-end/ElmohandesServiceCenter/src/main/java/com/example/elmohandesservicecenter/ElmohandesServiceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.elmohandesservicecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmohandesServiceCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmohandesServiceCenterApplication.class, args);
+    }
+
+}
